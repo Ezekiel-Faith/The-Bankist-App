ef311a19026dcff9c15f59dfd6ce1d9a8f2c595a
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-01 15:20:11 +0100

    implemented the delete account feature, using the findIndex()

--- js/script.js
+++ js/script.js
@@ -175,3 +175,25 @@ btnTransfer.addEventListener('click', (e) => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    // console.log(index);
+
+    // delete account
+    accounts.splice(index, 1);
+
+    // hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
